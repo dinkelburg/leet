811a171fdb098afce46b6f3b14ff4aee082a0e46
Ruben.Dinkelman <ruben@dinkelburg.nl>
2016-01-05 09:59:09 +0100

    96-PfSLocator

---
+++ 96-PfSLocator/Minor.ServiceBus.PfSLocatorService/Minor.ServiceBus.PfSLocatorService.DAL/ServiceLocationXMLDataMapper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Web.Hosting;
+
+namespace Minor.ServiceBus.PfSLocatorService
+{
+    public class ServiceLocationXmlDatamapper : IServiceLocationDatamapper
+    {
+        private string _filePath;
+
+        public ServiceLocationXmlDatamapper(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                _filePath = filePath;
+            }
+            else
+            {
+                throw new FilePathNotDefinedException("File path is not defined.");
+            }
+        }
+
+        public string FindMetadataEndpointAddress(string name, string profile)
+        {
+            return GetMetadataEndPointAdress(name, profile);
+        }
+
+        public string FindMetadataEndpointAddress(string name, string profile, decimal? version)
+        {
+            return GetMetadataEndPointAdress(name, profile, version);
+        }
+
+        public string GetMetadataEndPointAdress(string name, string profile, decimal? version = null)
+        {
+            var data = LoadXmlFile<locationData>();
+            var serviceLocationList = new List<string>();
+            foreach (var servicelocation in data.serviceLocation)
+            {
+                if (version != null)
+                {
+                    if (servicelocation.name == name && 
+                        servicelocation.profile == profile &&
+                        servicelocation.version == version)
+                    {
+                        serviceLocationList.Add(servicelocation.metadataAddress);
+                    }
+                } else if (servicelocation.name == name && servicelocation.profile == profile)
+                {
+                    if (servicelocation.version != null)
+                    {
+                        throw new VersionedRecordFoundException("The location service found has a version, so specify the version");
+                }
+                    serviceLocationList.Add(servicelocation.metadataAddress);
+                }
+            }
+            switch (serviceLocationList.Count())
+                {
+                case 0:
+                    throw new NoRecordsFoundException("No location services found");
+                case 1:
+                    return serviceLocationList.First();
+                default:
+                    throw new MultipleRecordsFoundException("Multiple location services found instead of one");
+            }
+        }
+
+        public T LoadXmlFile<T>()
+        {
+            string path = MakeAbsolutePath(_filePath);
+            XmlSerializer serializer = new XmlSerializer(typeof(locationData));
+            using (StreamReader reader = new StreamReader(path))
+            {
+                var data = serializer.Deserialize(reader);
+                return (T)data;
+            }
+        }
+
+        private static string MakeAbsolutePath(string relativePath)
+        {
+            string path = null;
+            if(HostingEnvironment.IsHosted)
+            {
+                path = HostingEnvironment.MapPath(@"~\" + relativePath);
+            }
+            else
+            {
+                string executionFolder = new DirectoryInfo(Directory.GetCurrentDirectory()).Name;
+                if (executionFolder == "Debug" || executionFolder == "Release")
+                {
+                    path = Directory.GetCurrentDirectory() + @"\..\..\" + relativePath;
+                } else
+                {
+                    path = Directory.GetCurrentDirectory() + @"\" + relativePath;
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ 96-PfSLocator/Minor.ServiceBus.PfSLocatorService/Minor.ServiceBus.PfSLocatorService.Implementation.Test/ServiceLocatorServiceWithoutVersionTest.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.ServiceModel;
+
+namespace Minor.ServiceBus.PfSLocatorService.Implementation.Test
+{
+    [TestClass]
+    public class ServiceLocatorServiceWithoutVersionTest
+    {
+        [TestMethod]
+        public void FindMetaEA_WithoutVersion_FoundSingle()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            string expected = "http://localhost:30412/BSKlantbeheer/mex";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile)).Returns(expected);
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            mock.Verify(m => m.FindMetadataEndpointAddress(name, profile), Times.Once);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<FunctionalErrorList>))]
+        public void FindMetaEA_WithoutVersion_FoundMultiple()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile))
+                .Throws(new MultipleRecordsFoundException("Multiple location services found instead of one"));
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+
+        [TestMethod]
+        public void FindMetaEA_WithoutVersion_FoundMultiple_ExcMessage()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile))
+                .Throws(new MultipleRecordsFoundException("Multiple location services found instead of one"));
+            var target = new ServiceLocatorService(mock.Object);
+
+            try
+            {
+                // Act
+                var result = target.FindMetadataEndpointAddress(serviceLocation);
+                Assert.Fail();
+            }
+            catch (FaultException<FunctionalErrorList> ex)
+            {
+                // Assert
+                Assert.AreEqual(1, ex.Detail.Details.Count);
+                Assert.AreEqual("Multiple location services found instead of one", ex.Detail.Details[0].Message);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<FunctionalErrorList>))]
+        public void FindMetaEA_WithoutVersion_FoundNone()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile))
+                .Throws(new NoRecordsFoundException("No location services found"));
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+
+        [TestMethod]
+        public void FindMetaEA_WithoutVersion_FoundNone_ExcMessage()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile))
+                .Throws(new NoRecordsFoundException("No location services found"));
+            var target = new ServiceLocatorService(mock.Object);
+
+            try
+            {
+                // Act
+                var result = target.FindMetadataEndpointAddress(serviceLocation);
+                Assert.Fail();
+            }
+            catch (FaultException<FunctionalErrorList> ex)
+            {
+                // Assert
+                Assert.AreEqual(1, ex.Detail.Details.Count);
+                Assert.AreEqual("No location services found", ex.Detail.Details[0].Message);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<FunctionalErrorList>))]
+        public void FindMetaEA_WithoutVersion_FoundVersion()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile))
+                .Throws(new VersionedRecordFoundException(
+                    "The location service found has a version, so specify the version"));
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+
+        [TestMethod]
+        public void FindMetaEA_WithoutVersion_FoundVersion_ExcMessage()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = "Development";
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile
+            };
+
+            var mock = new Mock<IServiceLocationDatamapper>(MockBehavior.Strict);
+            mock.Setup(m => m.FindMetadataEndpointAddress(name, profile))
+                .Throws(new VersionedRecordFoundException(
+                    "The location service found has a version, so specify the version"));
+            var target = new ServiceLocatorService(mock.Object);
+
+            try
+            {
+                // Act
+                var result = target.FindMetadataEndpointAddress(serviceLocation);
+                Assert.Fail();
+            }
+            catch (FaultException<FunctionalErrorList> ex)
+            {
+                // Assert
+                Assert.AreEqual(1, ex.Detail.Details.Count);
+                Assert.AreEqual("The location service found has a version, so specify the version", ex.Detail.Details[0].Message);
+            }
+        }
+    }
+}
