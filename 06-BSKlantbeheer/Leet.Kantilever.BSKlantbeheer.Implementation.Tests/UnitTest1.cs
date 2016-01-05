a911a32333a0156ab8de8f019b583a91abc53e94
DanielT <Dkthomassen@gmail.com>
2016-01-05 15:50:57 +0100

    Business service Klantbeheer aangemaakt

---
+++ 06-BSKlantbeheer/Leet.Kantilever.BSKlantbeheer.Implementation.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leet.Kantilever.BSKlantbeheer.Implementation.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
