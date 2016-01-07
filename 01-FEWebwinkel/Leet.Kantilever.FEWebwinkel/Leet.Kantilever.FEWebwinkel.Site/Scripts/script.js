81b1dfffd1bbeda3e3420c28d4acdb6581e96a93
Dylan van den Brink <dylanvandenbrink@live.nl>
2016-01-07 09:35:56 +0100

    Animatie gefinetuned

--- 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/Scripts/script.js
+++ 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/Scripts/script.js
@@ -1,29 +1,27 @@
 ﻿$(document).ready(function () {
     $('.mandBtn').click(function (eOpts) {
-
-
-        var $old = $(this);
-        //First we copy the arrow to the new table cell and get the offset to the document
-        var $new = $old.clone().appendTo('#winkelmand-counter');
+        // Get the clicked button
+        var $old = $(this);             
+        // Copy the element to the winkelmand element
+        var $new = $old.clone().appendTo('#winkelmand-target');
         var newOffset = $new.offset();
-        //Get the old position relative to document
+        // Get the old position relative to document
         var oldOffset = $old.offset();
-        //we also clone old to the document for the animation
+        // We also clone old to the document for the animation
         var $temp = $old.clone().appendTo('body');
-        //hide new and old and move $temp to position
-        //also big z-index, make sure to edit this to something that works with the page
+        // Hide the new element when it is in position
         $temp
           .css('position', 'absolute')
           .css('left', oldOffset.left)
           .css('top', oldOffset.top)
           .css('zIndex', 1000);
         $new.hide();
-        //animate the $temp to the position of the new img
-        $temp.animate({ 'top': newOffset.top, 'left': newOffset.left }, 'slow', function () {
-            //callback function, we remove $old and $temp and show $new
-            $new.show();
+        // animate the $temp to the winkelmand position
+        $temp.animate({ 'top': newOffset.top, 'left': newOffset.left, 'height': 0, 'width': 0 }, 'slow', function () {
+            // callback function, remove the temp element
             $temp.remove();
         });
+
         // Split 'prod-' from 'prod-123'
         var productId = this.id.split('-')[1];
         $('#winkelmand-counter').html(+$('#winkelmand-counter').html() + 1);
