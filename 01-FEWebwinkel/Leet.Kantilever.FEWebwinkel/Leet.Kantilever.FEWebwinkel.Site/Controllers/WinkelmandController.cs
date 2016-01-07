4535cb5dc4f8f2e7b3c56012616cf309991880ac
Dylan van den Brink <dylanvandenbrink@live.nl>
2016-01-07 11:28:26 +0100

    Ajax request toegevoegd die een controller action aanroept die uiteindelijk de backend gaat aanroepen

---
+++ 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/Controllers/WinkelmandController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace Leet.Kantilever.FEWebwinkel.Site.Controllers
+{
+    public class WinkelmandController : Controller
+    {
+        public HttpStatusCodeResult VoegProductToe(int ProductId, int Aantal)
+        {
+            // Call naar backend
+
+
+        
+            return new HttpStatusCodeResult(204);
+        }
+    }
+}
