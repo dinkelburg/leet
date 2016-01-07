24c0e54eafc467f70772086aa1007123a334b026
Bram Bruinsma <brambruinsma@gmail.com>
2016-01-07 15:52:41 +0100

    Begin gemaakt met verbinding naar BsCatalogus

---
+++ 03-PcSBestellen/Leet.Kantilever.PcSBestellen.Agent.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Leet.Kantilever.PcSBestellen.Agent.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetOneProductById()
+        {
+            AgentBSCatalogusBeheer agent = new AgentBSCatalogusBeheer();
+            try
+            {
+                var result = agent.GetProductsById(new int[] { 123 });
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Ophalen van producten faalde met de volgende parameters:", e);
+            }            
+        }
+    }
+}
---
+++ 03-PcSBestellen/Leet.Kantilever.PcSBestellen.Agent/AgentBSCatalogusBeheer.cs
@@ -0,0 +1,47 @@
+﻿using Kantilever.BsCatalogusbeheer.Messages.V1;
+using Kantilever.BsCatalogusbeheer.Product.V1;
+using Kantilever.BsCatalogusbeheer.V1;
+using Minor.ServiceBus.Agent.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet.Kantilever.PcSBestellen.Agent
+{
+    public class AgentBSCatalogusBeheer
+    {
+        private ServiceFactory<ICatalogusBeheer> _factory;
+
+        public AgentBSCatalogusBeheer()
+        {
+            _factory = new ServiceFactory<ICatalogusBeheer>("BSCatalogusBeheer");
+        }
+
+        public AgentBSCatalogusBeheer(ServiceFactory<ICatalogusBeheer> factory)
+        {
+            _factory = factory;
+        }
+
+        public IEnumerable<Product> GetProductsById(int[] ids)
+        {
+            List<Product> products = new List<Product>();
+            var agent = _factory.CreateAgent();
+            foreach(int id in ids)
+            {
+                var msg = new MsgFindProductByIdRequest() { Id = id };
+                var result = _factory.CreateAgent().FindProductById(msg);
+                if (result.Succes)
+                {
+                    products.Add(result.Product);
+                }
+                else
+                {
+                    throw new Exception(result.Foutmelding.Melding);
+                }
+            }
+            return products;            
+        }
+    }
+}
