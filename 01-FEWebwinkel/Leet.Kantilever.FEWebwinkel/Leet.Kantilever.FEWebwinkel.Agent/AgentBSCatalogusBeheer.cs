4cbf8dd3c4fd0e81027fd182451ee9a5782d1dfa
pim verheij <pim_verheij@msn.com>
2016-01-11 21:27:31 +0100

    Added Comments on AgentBSCatalogusBeheer

--- 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Agent/AgentBSCatalogusBeheer.cs
+++ 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Agent/AgentBSCatalogusBeheer.cs
@@ -14,20 +14,30 @@ namespace Leet.Kantilever.FEWebwinkel.Agent
     {
         private ServiceFactory<ICatalogusBeheer> _factory;
 
+        /// <summary>
+        /// Constructor to instantiate AgentBSCatalogusBeheer with a SerciceFactory
+        /// </summary>
         public AgentBSCatalogusBeheer()
         {
             _factory = new ServiceFactory<ICatalogusBeheer>("BSCatalogusBeheer");
         }
 
+        /// <summary>
+        /// Constructor to instantiate AgentBSCatalogusBeheer and to inject a SerciceFactory mock
+        /// </summary>
+        /// <param name="factory">ServiceFactory mock</param>
         public AgentBSCatalogusBeheer(ServiceFactory<ICatalogusBeheer> factory)
         {
             _factory = factory;
         }
 
+        /// <summary>
+        /// Get All product on a page in the BSCatalogusBeheer
+        /// </summary>
+        /// <param name="page">Page of the collection</param>
+        /// <returns>ProductCollection on the given page</returns>
         public ProductCollection FindProducts(int? page)
         {
-            
-
             var proxy = _factory.CreateAgent();
             var productCollection = proxy.FindProducts(new MsgFindProductsRequest
             {
