574280dee32bb4c45b82d7f7f220f4b9899ef81c
Roel Ruizendaal <r.ruizendaal@student.han.nl>
2016-01-12 12:31:51 +0100

    BSBestellingContext toegevoegd om deployment te testen

---
+++ 05-BSBestellingenbeheer/Leet.Kantilever.BSBestellingenbeheer.DAL.Tests/DALTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+
+namespace Leet.Kantilever.BSBestellingenbeheer.DAL.Tests
+{
+    [TestClass]
+    public class DALTest
+    {
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+            Database.SetInitializer(new BestellingDBInitializer());
+
+            using (var context = new BestellingContext())
+            {
+                context.Database.Initialize(false);
+            }
+
+        }
+
+        [TestMethod]
+        public void thisShouldDescribeTheTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            using (var context = new BestellingContext())
+            {
+                context.Bestellingen.Add(DummyDataDAL.GetBestelling());
+
+            }
+
+        }
+    }
+}
---
+++ 05-BSBestellingenbeheer/Leet.Kantilever.BSBestellingenbeheer.DAL/entities/Bestelling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leet.Kantilever.BSBestellingenbeheer.DAL.entities
+{
+    public class Bestelling
+    {
+        public long ID { get; set; }
+        public long Bestelnummer { get; set; }
+        public virtual ICollection<BestellingsRegel> Bestellingsregels { get; set; }
+        public DateTime Besteldatum { get; set; }
+        public long KlantID { get; set; }
+        public bool Ingepakt { get; set; }
+
+    }
+}
