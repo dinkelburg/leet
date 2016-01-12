4f8bca767168f62aa7b7ad3dfe4175aa88d86877
Pim Verheij <pimverheij@gmail.com>
2016-01-12 10:38:54 +0100

    Fix voor winkelmand layout

--- 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/App_Start/BundleConfig.cs
+++ 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/App_Start/BundleConfig.cs
@@ -29,6 +29,9 @@ namespace Leet.Kantilever.FEWebwinkel.Site
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Winkelmand").Include(
+                      "~/Content/Winkelmand.css"));
         }
     }
 }
