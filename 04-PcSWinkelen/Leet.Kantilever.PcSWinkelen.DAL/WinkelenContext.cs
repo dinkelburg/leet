eca912472c65f43949b966a138ad0a70d32e7eaa
Ruben.Dinkelman <ruben@dinkelburg.nl>
2016-01-12 12:05:11 +0100

    ..

--- 04-PcSWinkelen/Leet.Kantilever.PcSWinkelen.DAL/WinkelenContext.cs
+++ 04-PcSWinkelen/Leet.Kantilever.PcSWinkelen.DAL/WinkelenContext.cs
@@ -11,7 +11,7 @@ namespace Leet.Kantilever.PcSWinkelen.DAL
 {
     public class WinkelenContext : DbContext
     {
-        public WinkelenContext() : base() { }
+        public WinkelenContext() : base("Leet_PcSWinkelenDatabase") { }
         public DbSet<Product> Products { get; set; }
         public DbSet<Winkelmand> Winkelmanden { get; set; }
 
