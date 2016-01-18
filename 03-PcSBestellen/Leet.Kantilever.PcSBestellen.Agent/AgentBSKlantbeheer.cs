dbdb5735248835a875b207dafb99b1ea25c0adb0
Bram Bruinsma <brambruinsma@gmail.com>
2016-01-18 15:59:26 +0100

    BSKlantbeheer aan buildevent toegevoegd

---
+++ 03-PcSBestellen/Leet.Kantilever.PcSBestellen.Agent/AgentBSKlantbeheer.cs
@@ -0,0 +1,25 @@
+﻿using Leet.Kantilever.BSKlantbeheer.V1;
+using Minor.ServiceBus.Agent.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet.Kantilever.PcSBestellen.Agent
+{
+    public class AgentBSKlantbeheer : IAgentBSKlantbeheer
+    {
+        private ServiceFactory<IKlantbeheerService> _factory;
+
+        public AgentBSKlantbeheer()
+        {
+            _factory = new ServiceFactory<IKlantbeheerService>("BSKlantbeheer");
+        }
+
+        public AgentBSKlantbeheer(ServiceFactory<IKlantbeheerService> factory)
+        {
+            _factory = factory;
+        }
+    }
+}
---
+++ 03-PcSBestellen/Leet.Kantilever.PcSBestellen.Agent/IAgentBSKlantbeheer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet.Kantilever.PcSBestellen.Agent
+{
+    public interface IAgentBSKlantbeheer
+    {
+
+    }
+}
