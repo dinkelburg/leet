cfdddeef95accbd08ed5f2443f6e0603f02a2da7
Bas <bas.radem@gmail.com>
2016-01-18 11:24:27 +0100

    Unit tests toegevoegd.

---
+++ 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site.Tests/ViewModels/ProductVMTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leet.Kantilever.FEWebwinkel.Site.ViewModels;
+
+namespace Leet.Kantilever.FEWebwinkel.Site.Tests.ViewModels
+{
+    [TestClass]
+    public class ProductVMTests
+    {
+        [TestMethod]
+        public void ProductVM_PrijsInclusiefBtwGeeftGoedeWaarde()
+        {
+            //arrange
+            ProductVM p = new ProductVM
+            {
+                Prijs = 10m
+            };
+
+            //act
+            var result = p.PrijsInclusiefBtw;
+
+            //assert
+            Assert.AreEqual(12.10m, result);
+        }
+
+        [TestMethod]
+        public void ProductVM_PrijsInclusiefWerktMetNullPrijs()
+        {
+            //arrange
+            ProductVM p = new ProductVM();
+
+            //act
+            var result = p.PrijsInclusiefBtw;
+
+            //assert
+            Assert.AreEqual(null, result);
+        }
+    }
+}
