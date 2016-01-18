85e1cfd034bc53b8b1a3ee20eef104ede0584426
Bas <bas.radem@gmail.com>
2016-01-18 10:07:58 +0100

    Method omgedoopt naar nieuwe naam.

--- 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/Controllers/BestellenController.cs
+++ 01-FEWebwinkel/Leet.Kantilever.FEWebwinkel/Leet.Kantilever.FEWebwinkel.Site/Controllers/BestellenController.cs
@@ -67,7 +67,7 @@ namespace Leet.Kantilever.FEWebwinkel.Site.Controllers
                 AutoMapper.Mapper.CreateMap<KlantVM, Klant>();
                 var klant = AutoMapper.Mapper.Map<Klant>(klantVM);
                 klant.Klantnummer = clientID;
-                _bestellenAgent.KlantGegevensInvoeren(klant);
+                _bestellenAgent.CreateBestelling(klant);
 
                 return RedirectToRoute("ThankYou");
             }
